07dfeb8b52cb2812c662eebd68f39bcbf06f0945
Aleyeahhhh <ahmed.alaya@medtech.tn>
2023-07-24 11:12:31 +0100

    modified:   Factures/models.py 	modified:   Factures/utils.py    modified:   Factures/views.py

--- Factures/models.py
+++ Factures/models.py
@@ -48,104 +48,104 @@ class FactureModel(db.Model):
         db.session.delete(self)
         db.session.commit()
 
-class ForfaitModel(db.Model):
-    __tablename__ = 'forfaitaire'
-    id_facture= db.Column(db.Integer, primary_key=True)
-    TypeFacture = db.Column(db.Enum(TypeFacture), nullable=True)
-    date_emission = db.Column(db.DateTime, default=datetime.utcnow, nullable=True)
-    description = db.Column(db.String(100), nullable=False)
-    prix_ht = db.Column(db.Float, nullable=False)
-    prix_forfaitaire = db.Column(db.Float, nullable=True)
-    total = db.Column(db.Float, nullable=False)
-    tva = db.Column(db.Float, nullable=False)
-    total_ttc = db.Column(db.Float, nullable=False)
+# class ForfaitModel(db.Model):
+#     __tablename__ = 'forfaitaire'
+#     id_facture= db.Column(db.Integer, primary_key=True)
+#     TypeFacture = db.Column(db.Enum(TypeFacture), nullable=True)
+#     date_emission = db.Column(db.DateTime, default=datetime.utcnow, nullable=True)
+#     description = db.Column(db.String(100), nullable=False)
+#     prix_ht = db.Column(db.Float, nullable=False)
+#     prix_forfaitaire = db.Column(db.Float, nullable=True)
+#     total = db.Column(db.Float, nullable=False)
+#     tva = db.Column(db.Float, nullable=False)
+#     total_ttc = db.Column(db.Float, nullable=False)
     
 
-    def serialize(self):
-        data = {
-            'id_facture': self.id,
-            'TypeFacture': self.TypeFacture,
-            'date_emission': self.created_at.strftime('%Y-%m-%d %H:%M:%S'),
-            'description':self.description,
-            'prix_ht':self.prix_ht,
-            'prix_forfaitaire':self.prix_forfaitaire,
-            'total': self.total,
-            'tva': self.tva,
-            'total_ttc':self.total_ttc,
-            }
-        return data
-    def save_to_db(self):
-        db.session.add(self)
-        db.session.commit()
+#     def serialize(self):
+#         data = {
+#             'id_facture': self.id,
+#             'TypeFacture': self.TypeFacture,
+#             'date_emission': self.created_at.strftime('%Y-%m-%d %H:%M:%S'),
+#             'description':self.description,
+#             'prix_ht':self.prix_ht,
+#             'prix_forfaitaire':self.prix_forfaitaire,
+#             'total': self.total,
+#             'tva': self.tva,
+#             'total_ttc':self.total_ttc,
+#             }
+#         return data
+#     def save_to_db(self):
+#         db.session.add(self)
+#         db.session.commit()
 
-    def delete_from_db(self):
-        db.session.delete(self)
-        db.session.commit()
-class JourHommeModel(db.Model):
-    __tablename__= 'Jour_Homme'
-    id_facture = db.Column(db.Integer,primary_key=True)
-    date_emission = db.Column(db.DateTime, default=datetime.utcnow, nullable=True)
-    description = db.Column(db.String(100), nullable=False)
-    prix_ht = db.Column(db.Float, nullable=False)
-    TypeFacture = db.Column(db.Enum(TypeFacture), nullable=True)
-    prix_jour = db.Column(db.Float, nullable=True)
-    nbr_jour = db.Column(db.Integer, nullable=True)
-    total = db.Column(db.Float, nullable=False)
-    tva = db.Column(db.Float, nullable=False)
-    total_ttc = db.Column(db.Float, nullable=False)
-    def serialize(self):
-         data = {
-        'id_facture':self.id,
-        'TypeFacture': self.TypeFacture,
-        'date_emission': self.created_at.strftime('%Y-%m-%d %H:%M:%S'),
-        'description':self.description,
-        'prix_ht':self.prix_ht,
-        'prix_forfaitaire':self.prix_forfaitaire,
-        'total': self.total,
-        'tva': self.tva,
-        'total_ttc':self.total_ttc,
+#     def delete_from_db(self):
+#         db.session.delete(self)
+#         db.session.commit()
+# class JourHommeModel(db.Model):
+#     __tablename__= 'Jour_Homme'
+#     id_facture = db.Column(db.Integer,primary_key=True)
+#     date_emission = db.Column(db.DateTime, default=datetime.utcnow, nullable=True)
+#     description = db.Column(db.String(100), nullable=False)
+#     prix_ht = db.Column(db.Float, nullable=False)
+#     TypeFacture = db.Column(db.Enum(TypeFacture), nullable=True)
+#     prix_jour = db.Column(db.Float, nullable=True)
+#     nbr_jour = db.Column(db.Integer, nullable=True)
+#     total = db.Column(db.Float, nullable=False)
+#     tva = db.Column(db.Float, nullable=False)
+#     total_ttc = db.Column(db.Float, nullable=False)
+#     def serialize(self):
+#          data = {
+#         'id_facture':self.id,
+#         'TypeFacture': self.TypeFacture,
+#         'date_emission': self.created_at.strftime('%Y-%m-%d %H:%M:%S'),
+#         'description':self.description,
+#         'prix_ht':self.prix_ht,
+#         'prix_forfaitaire':self.prix_forfaitaire,
+#         'total': self.total,
+#         'tva': self.tva,
+#         'total_ttc':self.total_ttc,
           
-     }
-         return data
-    def save_to_db(self):
-        db.session.add(self)
-        db.session.commit()
+#      }
+#          return data
+#     def save_to_db(self):
+#         db.session.add(self)
+#         db.session.commit()
 
-    def delete_from_db(self):
-        db.session.delete(self)
-        db.session.commit()
-class LivrableModel(db.Model):
-    __tablename__ = 'Livrables'
-    id_facture=db.Column(db.Integer, primary_key=True)
-    date_emission = db.Column(db.DateTime, default=datetime.utcnow, nullable=True)
-    description = db.Column(db.String(100), nullable=False)
-    prix_ht = db.Column(db.Float, nullable=False)
-    TypeFacture = db.Column(db.Enum(TypeFacture), nullable=True)
-    total = db.Column(db.Float, nullable=False)
-    tva = db.Column(db.Float, nullable=False)
-    total_ttc = db.Column(db.Float, nullable=False)
-    prix_livrable = db.Column(db.Float, nullable=True)
-    def serialize(self):
-        data = {
-            'id_facture':self.id,
-            'TypeFacture': self.TypeFacture,
-            'date_emission': self.created_at.strftime('%Y-%m-%d %H:%M:%S'),
-            'description':self.description,
-            'prix_ht':self.prix_ht,
-            'prix_livrable':self.prix_livrable,
-            'total': self.total,
-            'tva': self.tva,
-            'total_ttc':self.total_ttc,
+#     def delete_from_db(self):
+#         db.session.delete(self)
+#         db.session.commit()
+# class LivrableModel(db.Model):
+#     __tablename__ = 'Livrables'
+#     id_facture=db.Column(db.Integer, primary_key=True)
+#     date_emission = db.Column(db.DateTime, default=datetime.utcnow, nullable=True)
+#     description = db.Column(db.String(100), nullable=False)
+#     prix_ht = db.Column(db.Float, nullable=False)
+#     TypeFacture = db.Column(db.Enum(TypeFacture), nullable=True)
+#     total = db.Column(db.Float, nullable=False)
+#     tva = db.Column(db.Float, nullable=False)
+#     total_ttc = db.Column(db.Float, nullable=False)
+#     prix_livrable = db.Column(db.Float, nullable=True)
+#     def serialize(self):
+#         data = {
+#             'id_facture':self.id,
+#             'TypeFacture': self.TypeFacture,
+#             'date_emission': self.created_at.strftime('%Y-%m-%d %H:%M:%S'),
+#             'description':self.description,
+#             'prix_ht':self.prix_ht,
+#             'prix_livrable':self.prix_livrable,
+#             'total': self.total,
+#             'tva': self.tva,
+#             'total_ttc':self.total_ttc,
           
-        }
-        return data
-    def save_to_db(self):
-        db.session.add(self)
-        db.session.commit()
+#         }
+#         return data
+    # def save_to_db(self):
+    #     db.session.add(self)
+    #     db.session.commit()
 
-    def delete_from_db(self):
-        db.session.delete(self)
-        db.session.commit()
+    # def delete_from_db(self):
+    #     db.session.delete(self)
+    #     db.session.commit()
 
 #
  #def serialize(self):
--- Factures/utils.py
+++ Factures/utils.py
@@ -1,13 +1,13 @@
-from Factures.models import FactureModel,ForfaitModel,JourHommeModel,LivrableModel, TypeFacture
+from Factures.models import FactureModel, TypeFacture
 from db import db
 from enum import Enum
 
 
-#def get_all_factures():
+# def get_all_factures():
 #    return {'facture': list(map(lambda x: x.serialize(), FactureModel.query.all()))}
 
  
-#def get_facture(_id_facture):
+# def get_facture(_id_facture):
 #    return {'facture': list(map(lambda x: x.serialize(), FactureModel.query.filter_by(id_facture=_id_facture).first()))}
 
 def get_all_factures():
@@ -21,15 +21,15 @@ def get_facture(_id):
     return {'message': 'facture not found.'}, 404
 def create_facture(id_facture, TypeFacture, date_emission, description, prix_ht, prix_forfaitaire, prix_jour, nbr_jour, prix_livrable, total, tva, total_ttc):
     if TypeFacture == 'FORFAIT':
-        facture = ForfaitModel(id_facture=id_facture, TypeFacture=TypeFacture, date_emission=date_emission, description=description, prix_ht=prix_ht, prix_forfaitaire=prix_forfaitaire, total=total, tva=tva, total_ttc=total_ttc)
+        facture = FactureModel(id_facture=id_facture, TypeFacture=TypeFacture, date_emission=date_emission, description=description, prix_ht=prix_ht, prix_forfaitaire=prix_forfaitaire, total=total, tva=tva, total_ttc=total_ttc)
         facture.save_to_db()
         return id_facture
     elif TypeFacture == 'JOUR_HOMME':
-        facture = JourHommeModel(id_facture=id_facture, TypeFacture=TypeFacture, date_emission=date_emission, description=description, prix_ht=prix_ht, prix_jour=prix_jour, nbr_jour=nbr_jour, total=total, tva=tva, total_ttc=total_ttc)
+        facture = FactureModel(id_facture=id_facture, TypeFacture=TypeFacture, date_emission=date_emission, description=description, prix_ht=prix_ht, prix_jour=prix_jour, nbr_jour=nbr_jour, total=total, tva=tva, total_ttc=total_ttc)
         facture.save_to_db()
         return id_facture
     elif TypeFacture == 'LIVRABLES':
-        facture = LivrableModel(id_facture=id_facture, TypeFacture=TypeFacture, date_emission=date_emission, description=description, prix_ht=prix_ht, prix_livrable=prix_livrable, total=total, tva=tva, total_ttc=total_ttc)
+        facture = FactureModel(id_facture=id_facture, TypeFacture=TypeFacture, date_emission=date_emission, description=description, prix_ht=prix_ht, prix_livrable=prix_livrable, total=total, tva=tva, total_ttc=total_ttc)
         facture.save_to_db()
         return id_facture
     else:
@@ -59,25 +59,26 @@ def create_facture(id_facture, TypeFacture, date_emission, description, prix_ht,
 #     db.session.add(facture)
 #     db.session.commit()
 #     return
-def update_facture(_id_facture, TypeFacture,date_emission,description, prix_ht,prix_forfaitaire,prix_jour, nbr_jour,prix_livrable,total,tva, total_ttc):
+
     
+def update_facture(_id_facture, TypeFacture, date_emission, description, prix_ht, prix_forfaitaire, prix_jour, nbr_jour, prix_livrable, total, tva, total_ttc):
     facture = FactureModel.query.get(_id_facture)
     if facture:
-        facture._id_facture = _id_facture,
-        facture.TypeFacture = TypeFacture,
-        facture.date_emission = date_emission,
-        facture.description=description, 
-        facture.prix_ht=prix_ht,
-        facture.prix_forfaitaire=prix_forfaitaire,
-        facture.prix_jour=prix_jour,
-        facture.nbr_jour=nbr_jour,
-        facture.prix_livrable=prix_livrable,
-        facture.total=total,
-        facture.tva=tva, 
-        facture.total_ttc=total_ttc,
+        facture.TypeFacture = TypeFacture
+        facture.date_emission = date_emission
+        facture.description = description
+        facture.prix_ht = prix_ht
+        facture.prix_forfaitaire = prix_forfaitaire
+        facture.prix_jour = prix_jour
+        facture.nbr_jour = nbr_jour
+        facture.prix_livrable = prix_livrable
+        facture.total = total
+        facture.tva = tva
+        facture.total_ttc = total_ttc
         db.session.commit()
         return {'message': 'facture updated successfully.'}
     return {'message': 'facture not found.'}, 404
+
 
 def delete_facture(_id):
     facture = FactureModel.query.get(_id)
